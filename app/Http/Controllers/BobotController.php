42c0a784ca13a98beb384d74039945fe49dcbade
Karina May <126779482+KarinaMay0@users.noreply.github.com>
2024-07-24 10:04:16 +0700

    Update BobotController.php

--- app/Http/Controllers/BobotController.php
+++ app/Http/Controllers/BobotController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Bobot; 
-
 use Illuminate\Http\Request;
-//import return type View
 use Illuminate\View\View;
 
 class BobotController extends Controller
@@ -22,36 +20,6 @@ class BobotController extends Controller
         $bobots = Bobot::latest()->paginate(10);
 
        //render view with products
-      
        return view('bobots.index', compact('bobots'));
-
-    }
-
-    
-}
-
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\Bobot;
-use Illuminate\Http\Request;
-use Illuminate\View\View;
-
-class BobotController extends Controller
-{
-    //
-     /**
-     * index
-     *
-     * @return void
-     */
-    public function index() : View
-    {
-        //get all products
-        $bobots = Bobot::latest()->paginate(10);
-
-        //render view with products
-        return view('bobots.index', compact('bobots'));
-    }
+    }  
 }
