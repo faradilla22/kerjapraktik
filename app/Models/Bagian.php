28c967d78aac4191e6f019b1a38c17511f62a176
Karina May <126779482+KarinaMay0@users.noreply.github.com>
2024-07-24 10:08:27 +0700

    Perbaikan Bagian.php

--- app/Models/Bagian.php
+++ app/Models/Bagian.php
@@ -29,35 +29,3 @@ class Bagian extends Model
         return $this->hasMany(Barang::class, 'id_bagian');
     }
 }
-
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Bagian extends Model
-{
-    use HasFactory;
-
-    /**
-     * fillable
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'nama_bagian',
-        'id_pabrik',
-    ];
-
-    public $timestamps = false;
-
-    public function pabrik() {
-        return $this->belongsTo(Pabrik::class, 'id_pabrik');
-    }
-
-    public function barang() {
-        return $this->hasMany(Barang::class, 'id_bagian');
-    }
-}
