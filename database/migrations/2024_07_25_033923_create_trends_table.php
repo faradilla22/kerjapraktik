5f88031225c38da38eff20f13e0043bb43b8c819
Faradilla Maulia <faradillamaulia22@gmail.com>
2024-07-25 19:43:13 +0700

    menambahkan migrasi data trend

---
+++ database/migrations/2024_07_25_033923_create_trends_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trends', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->float('R', 8, 2);
+            $table->unsignedBigInteger('id_barang');
+
+
+            $table->foreign('id_barang')->references('id')->on('barangs')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trends');
+    }
+};
